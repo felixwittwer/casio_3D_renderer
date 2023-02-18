fb66a679faa5bb7a0a2f551a99141038b7e30597
Felix Wittwer <120730441+felixwittwer@users.noreply.github.com>
2023-02-18 15:31:06 +0100

    Initial commit

---
+++ 3DCUBE.c
@@ -0,0 +1,118 @@
+/*****************************************************************/
+/*                                                               */
+/*   CASIO fx-9860G SDK Library                                  */
+/*                                                               */
+/*   File name : [ProjectName].c                                 */
+/*                                                               */
+/*   Copyright (c) 2006 CASIO COMPUTER CO., LTD.                 */
+/*                                                               */
+/*****************************************************************/
+#include "fxlib.h"
+#include <stdio.h>
+#include <math.h>
+#include <mathf.h>
+
+void DrawCube(int x, int y, int heigth, int rotatex , int rotatey, int rotatez, int radius, float stretchx, float stretchy, int debug){
+    int iteration = 0;
+    float rad = 0;
+    float radone = 0;
+    float radtwo = 0;
+
+    if(debug==1){
+        while(iteration<360){
+            Bdisp_SetPoint_VRAM(stretchx*(radius*sin(iteration))+x, stretchy*(radius*cos(iteration))+y, 1);
+            Bdisp_SetPoint_VRAM(stretchx*(radius*sin(iteration))+x, stretchy*(radius*cos(iteration))+y+heigth, 1);
+            iteration = iteration + 1;
+        }
+    }
+
+    //draw vertical lines of cube
+    iteration = 0;
+    while(iteration<361){
+        rad = ((iteration+rotatez)/180.0)*3.141592;
+        Bdisp_DrawLineVRAM(stretchx*(radius*sin(rad))+x, stretchy*(radius*cos(rad))+y, stretchx*(radius*sin(rad))+x, stretchy*(radius*cos(rad))+y+heigth);
+        iteration = iteration +90;
+    }
+
+    //conect vertical lines
+    iteration = 0;
+    while(iteration<361){
+        radone = ((iteration+rotatez)/180.0)*3.141592;
+        radtwo = (((iteration+90)+rotatez)/180.0)*3.141592;
+        Bdisp_DrawLineVRAM(stretchx*(radius*sin(radone))+x, stretchy*(radius*cos(radone))+y+heigth, stretchx*(radius*sin(radtwo))+x, stretchy*(radius*cos(radtwo))+y+heigth);
+        Bdisp_DrawLineVRAM(stretchx*(radius*sin(radone))+x, stretchy*(radius*cos(radone))+y, stretchx*(radius*sin(radtwo))+x, stretchy*(radius*cos(radtwo))+y);
+        iteration = iteration +90;
+    }
+
+    // Bdisp_SetPoint_VRAM(stretchx*(radius*sin(rad))+x, stretchy*(radius*cos(rad))+y+10, 1);
+}
+
+//****************************************************************************
+//  AddIn_main (Sample program main function)
+//
+//  param   :   isAppli   : 1 = This application is launched by MAIN MENU.
+//                        : 0 = This application is launched by a strip in eACT application.
+//
+//              OptionNum : Strip number (0~3)
+//                         (This parameter is only used when isAppli parameter is 0.)
+//
+//  retval  :   1 = No error / 0 = Error
+//
+//****************************************************************************
+int AddIn_main(int isAppli, unsigned short OptionNum)
+{
+    unsigned int key;
+    int rotatez = 20;
+    float stretchx = 1;
+    float stretchy = 0.3;
+    int sidelength = 20;
+
+    Bdisp_AllClr_DDVRAM();
+
+    while(1){
+        GetKey(&key);
+        Bdisp_AllClr_DDVRAM();
+        if(key==KEY_CTRL_RIGHT){
+            rotatez = rotatez + 10;
+        }else if(key==KEY_CTRL_LEFT){
+            rotatez = rotatez - 10;
+        }
+        DrawCube(60,20,sidelength,0,0,rotatez,sidelength,stretchx,stretchy,0);
+    }
+
+    return 1;
+}
+
+
+//****************************************************************************
+//**************                                              ****************
+//**************                 Notice!                      ****************
+//**************                                              ****************
+//**************  Please do not change the following source.  ****************
+//**************                                              ****************
+//****************************************************************************
+
+
+#pragma section _BR_Size
+unsigned long BR_Size;
+#pragma section
+
+
+#pragma section _TOP
+
+//****************************************************************************
+//  InitializeSystem
+//
+//  param   :   isAppli   : 1 = Application / 0 = eActivity
+//              OptionNum : Option Number (only eActivity)
+//
+//  retval  :   1 = No error / 0 = Error
+//
+//****************************************************************************
+int InitializeSystem(int isAppli, unsigned short OptionNum)
+{
+    return INIT_ADDIN_APPLICATION(isAppli, OptionNum);
+}
+
+#pragma section
+
